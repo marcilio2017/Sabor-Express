36aa8e675d3a72a680ad7fff7be04db37b83b454
Marcílio Bezerra <marcilio.bc@gmail.com>
2024-02-09 16:36:55 -0300

    Projeto inicial
    Antes das funções.

---
+++ app.py
@@ -0,0 +1,13 @@
+print ('Sabor Express\n')
+
+print ('1 - Cadastrar Restaurante')
+
+print ('2 - Listar Restaurante')
+print ('3 - Ativar Restaurante')
+print ('4 - Sair\n')
+
+opcao_escolhida = int(input('Escolha uma opção: '))
+print(opcao_escolhida)
+
+
+
