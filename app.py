88d2b546c2385855869049fbcd0de8ea4db4ad37
Marcílio Bezerra <marcilio.bc@gmail.com>
2024-02-10 14:27:44 -0300

    IF`s

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 print ('Sabor Express\n')
 
 print ('1 - Cadastrar Restaurante')
-
 print ('2 - Listar Restaurante')
 print ('3 - Ativar Restaurante')
 print ('4 - Sair\n')
 
 opcao_escolhida = int(input('Escolha uma opção: '))
 print(opcao_escolhida)
+
+if opcao_escolhida == 1:
+    print('Restaurante Cadastrado:')
+elif opcao_escolhida == 2:
+    print ('Listar Restaurantes:')
+elif opcao_escolhida == 3:
+    print ('Ativar Restaurante:')
+else:
+    print ('Sair')
+
+    
 
 
 
