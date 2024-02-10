1206fbfe68c6c8f0d1360ced09901f1ac82d4f34
Marcílio Bezerra <marcilio.bc@gmail.com>
2024-02-10 18:47:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-print ('Sabor Express\n')
+import os
 
-print ('1 - Cadastrar Restaurante')
-print ('2 - Listar Restaurante')
-print ('3 - Ativar Restaurante')
-print ('4 - Sair\n')
+def exibir_nome_do_programa():
+    print ('Sabor Express\n')
+
+def exibir_opcoes():
+
+    print ('1 - Cadastrar Restaurante')
+    print ('2 - Listar Restaurante')
+    print ('3 - Ativar Restaurante')
+    print ('4 - Sair\n')
+
+def finalizar_app():
+    os.system('cls')#limpando a tela
+    print('Encerrando app.\n')
+
 
 opcao_escolhida = int(input('Escolha uma opção: '))
 print(opcao_escolhida)
@@ -15,7 +25,14 @@ elif opcao_escolhida == 2:
 elif opcao_escolhida == 3:
     print ('Ativar Restaurante:')
 else:
-    print ('Sair')
+    finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+
+if __name__ == '__main__':
+    main()
 
     
 
